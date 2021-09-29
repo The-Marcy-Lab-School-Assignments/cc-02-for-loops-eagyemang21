c29338741d18ab7164e6349c5815ce1235504fe9
EC2 Default User <ec2-user@ip-172-31-27-40.us-east-2.compute.internal>
2021-09-29 14:49:52 +0000

    Elijah code challenge 9/29/2021

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
 //1
 function countToTen(){
-  
+  for(let i = 1; i <= 10; i++)
+  console.log(i);
 }
+countToTen();
 
 //2
-function countFromOne(){
-  
+function countFromOne(number){
+  for(let i = 1; i <= number; i++)
+  console.log(i);
 }
+countFromOne(7);
 
 //3
-function countEveryEven(){
-  
+function countEveryEven(number){
+  for(let i = 2; i <= number; i+=2)
+  console.log(i) 
 }
+countEveryEven(13)
 
 //4
-function countEveryOdd(){
-  
+function countEveryOdd(number){
+  for(let i = 1; i <= number; i+=2)
+  console.log(i);
 }
+countEveryOdd(16)
 
 //5 
-function countEvens(){
-  
+function countEvens(numbers){
+ let count = 0
+ for(let i = 0; i < numbers.length; i++){
+    if(numbers[i] % 2 ===0){
+        count+=1
+    }
+ }
+ return count
 }
-
+console.log(countEvens([2,4,5,6,19,22,24,27]))
